8d634caa48c7e1d3ec249b8b4768d5ac0c7931c7
Maksim Kutishchev <max@amopro.ru>
2019-11-19 15:26:58 +0400

    fix not defined properties (`use_season_discount`,`season_discount_sign`) in Price entity

--- src/Entities/Price.php
+++ src/Entities/Price.php
@@ -9,13 +9,17 @@ namespace Rtoperator\Entities;
  * @property string $title
  * @property float $sum_for_insider
  * @property float $sum_for_foreigner
+ * @property bool $use_season_discount
+ * @property int $season_discount_sign
  */
 class Price extends AbstractEntity
 {
     protected $casts = [
-        'tourId' => 'int',
-        'title' => 'string',
-        'sum_for_insider' => 'float',
-        'sum_for_foreigner' => 'float',
+        'tourId'               => 'int',
+        'title'                => 'string',
+        'sum_for_insider'      => 'float',
+        'sum_for_foreigner'    => 'float',
+        'use_season_discount'  => 'boolean',
+        'season_discount_sign' => 'int'
     ];
 }
